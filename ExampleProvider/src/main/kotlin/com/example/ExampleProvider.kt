534b11a62b05bfb70c560cbe37d8538b9d1abbfe
68resull68-jpg <68resull68@gmail.com>
2026-08-15 01:27:22 +0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,9 +1,11 @@
 package com.example
 
+import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.newMovieLoadResponse
 import com.lagradost.cloudstream3.newMovieSearchResponse
 import org.json.JSONObject
 import java.net.URLEncoder
@@ -22,7 +24,7 @@ class ExampleProvider : MainAPI() {
         val encoded = URLEncoder.encode(query, "UTF-8")
 
         val url =
-            "https://archive.org/advancedsearch.php" +
+            "$mainUrl/advancedsearch.php" +
             "?q=title%3A%28$encoded%29+AND+mediatype%3Amovies" +
             "&fl%5B%5D=identifier" +
             "&fl%5B%5D=title" +
@@ -58,12 +60,41 @@ class ExampleProvider : MainAPI() {
                     type = TvType.Movie
                 ) {
                     this.year = year
-                    this.posterUrl =
-                        "$mainUrl/services/img/$identifier"
+                    this.posterUrl = "$mainUrl/services/img/$identifier"
                 }
             )
         }
 
         return results
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val identifier = url.substringAfterLast("/details/")
+
+        val response = app.get(
+            "$mainUrl/metadata/$identifier"
+        ).text
+
+        val json = JSONObject(response)
+
+        val title = json.optString("title")
+            .ifBlank { identifier }
+
+        val description = json.optString("description")
+            .takeIf { it.isNotBlank() }
+
+        val year = json.optInt("year", 0)
+            .takeIf { it > 0 }
+
+        return newMovieLoadResponse(
+            name = title,
+            url = url,
+            type = TvType.Movie,
+            dataUrl = url
+        ) {
+            this.posterUrl = "$mainUrl/services/img/$identifier"
+            this.plot = description
+            this.year = year
+        }
     }
 }
