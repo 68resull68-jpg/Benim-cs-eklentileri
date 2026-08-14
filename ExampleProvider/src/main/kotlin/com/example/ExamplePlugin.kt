29a7ee63271f2a4e4e70d3c749e0b1ecddda2523
68resull68-jpg <68resull68@gmail.com>
2026-08-15 00:25:42 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,24 @@
 package com.example
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class BenimCSPlugin : Plugin() {
+class ExamplePlugin : Plugin() {
+    private var activity: AppCompatActivity? = null
 
     override fun load(context: Context) {
+        activity = context as? AppCompatActivity
+
         registerMainAPI(ExampleProvider())
+
+        openSettings = {
+            val frag = BlankFragment(this)
+            activity?.let {
+                frag.show(it.supportFragmentManager, "Frag")
+            }
+        }
     }
 }
