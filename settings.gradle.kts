997deb996a5316ec6f87bca10a94448f90849335
68resull68-jpg <68resull68@gmail.com>
2026-08-16 01:39:53 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        maven("https://repo.lagradost.cloud/releases")
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "Cloudstream-Repo"
 include(":app")
 include(":DizipalProvider")
