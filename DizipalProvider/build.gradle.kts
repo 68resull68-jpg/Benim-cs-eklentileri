201dbfcb3dd6cfc8d815056caefe08276c28d1f7
68resull68-jpg <68resull68@gmail.com>
2026-08-16 02:49:51 +0300

    Update build.gradle.kts

--- DizipalProvider/build.gradle.kts
+++ DizipalProvider/build.gradle.kts
@@ -1,13 +1,3 @@
-plugins {
-    kotlin("jvm") version "2.0.20"
-}
-
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://repo.lagradost.cloud/releases")
-}
-
 dependencies {
     compileOnly("com.lagradost:cloudstream:1.0")
 }
