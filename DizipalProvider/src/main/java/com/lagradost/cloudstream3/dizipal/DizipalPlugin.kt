ee9641d3d351f03434805ed7793d1dec09d4bf89
68resull68-jpg <68resull68@gmail.com>
2026-08-16 03:03:47 +0300

    Update DizipalPlugin.kt

--- DizipalProvider/src/main/java/com/lagradost/cloudstream3/dizipal/DizipalPlugin.kt
+++ DizipalProvider/src/main/java/com/lagradost/cloudstream3/dizipal/DizipalPlugin.kt
@@ -1,15 +1,7 @@
 package com.lagradost.cloudstream3.dizipal
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.utils.newEpisode
-import com.lagradost.cloudstream3.utils.newHomePageResponse
-import com.lagradost.cloudstream3.utils.newMovieLoadResponse
-import com.lagradost.cloudstream3.utils.newMovieSearchResponse
-import com.lagradost.cloudstream3.utils.newTvSeriesLoadResponse
-import com.lagradost.cloudstream3.utils.newTvSeriesSearchResponse
-import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.utils.*
 
 class DizipalProvider : MainAPI() {
     override var mainUrl = "https://dizipal818.com"
@@ -28,7 +20,7 @@ class DizipalProvider : MainAPI() {
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(if(page == 1) request.data else "${request.data}page/$page/").document
         val home = document.select("div.col-md-2.col-sm-3.col-6").mapNotNull { it.toSearchResult() }
-        return newHomePageResponse(request.name, home)
+        return HomePageResponse(request.name, home) // newHomePageResponse -> HomePageResponse
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
@@ -44,9 +36,9 @@ class DizipalProvider : MainAPI() {
         val isSeries = href.contains("/dizi-")
         
         return if(isSeries) {
-            newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = poster }
+            TvSeriesSearchResponse(title, href, TvType.TvSeries, posterUrl = poster) // newTvSeriesSearchResponse -> TvSeriesSearchResponse
         } else {
-            newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
+            MovieSearchResponse(title, href, TvType.Movie, posterUrl = poster) // newMovieSearchResponse -> MovieSearchResponse
         }
     }
 
@@ -59,29 +51,21 @@ class DizipalProvider : MainAPI() {
         
         val episodeElements = document.select("div.episode a, ul li.episode a")
         val episodes = episodeElements.map { 
-            newEpisode(it.attr("href")) {
-                name = it.text().trim()
-            }
+            Episode(it.attr("href"), name = it.text().trim()) // newEpisode -> Episode
         }.reversed()
 
         return if(episodes.isEmpty()) {
-            newMovieLoadResponse(title, url, TvType.Movie, url) {
-                this.posterUrl = poster
-                this.plot = plot
-            }
+            MovieLoadResponse(title, url, TvType.Movie, url, posterUrl = poster, plot = plot) // newMovieLoadResponse -> MovieLoadResponse
         } else {
-            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
-                this.posterUrl = poster
-                this.plot = plot
-            }
+            TvSeriesLoadResponse(title, url, TvType.TvSeries, episodes, posterUrl = poster, plot = plot) // newTvSeriesLoadResponse -> TvSeriesLoadResponse
         }
     }
 
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
-        subtitleCallback: Callback<SubtitleFile>,
-        callback: Callback<ExtractorLink>
+        subtitleCallback: (SubtitleFile) -> Unit, // Callback<SubtitleFile> -> (SubtitleFile) -> Unit
+        callback: (ExtractorLink) -> Boolean // Callback<ExtractorLink> -> (ExtractorLink) -> Boolean
     ): Boolean {
         val document = app.get(data).document
         document.select("iframe").forEach {
